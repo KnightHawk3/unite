34b419d558aaf86f51b743297c49623290c7839c
Melody Kelly <melody@melody.blue>
2016-11-02 03:23:53 +1000

    I am that one person who commits text eidotr editor files because their useful

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;; Pony mode config for the megacorp project
+((nil . ;; This applies these settings regardless of major mode
+
+  ((pony-settings (make-pony-project
+                   :python "/home/melody/.virtualenvs/unite/bin/python"
+                   :settings "settings.dev")
+))))
